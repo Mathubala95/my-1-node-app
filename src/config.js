6e08703bbef984223476b8d192458f387f9adcea
Mathubala95 <madhuvalar.govindaraj@gmail.com>
2022-11-12 17:08:31 +0530

    mnt2

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export  const config = {
-    // api : "http://localhost:3003" this is for db.js//
-    api : "https://node1-project.herokuapp.com/"
+    //  this is for db.js//
+    // api : "https://node1-project.herokuapp.com/"
+    api : "http://localhost:3003"
 }
